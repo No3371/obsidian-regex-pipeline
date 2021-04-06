461ffd40c818bfedf2a33de2b2903ebd9124fb2a
RenChen <renchenpro@gmail.com>
2021-04-06 18:29:17 +0800

    First step

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as fs from 'fs'
+import { App, ButtonComponent, Component, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, Vault } from 'obsidian';
+import * as path from 'path';
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -10,21 +12,22 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	rules: string[]
 
 	async onload() {
 		console.log('loading plugin');
 
 		await this.loadSettings();
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
+		this.addRibbonIcon('dice', 'Regex Rulesets', () => {
+			new ApplyRuleSetMenu(this.app, this).open();
 		});
 
 		this.addStatusBarItem().setText('Status Bar Text');
 
 		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
+			id: 'apply-ruleset',
+			name: 'Apply Ruleset',
 			// callback: () => {
 			// 	console.log('Simple Callback');
 			// },
@@ -32,7 +35,7 @@ export default class MyPlugin extends Plugin {
 				let leaf = this.app.workspace.activeLeaf;
 				if (leaf) {
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new ApplyRuleSetMenu(this.app, this).open();
 					}
 					return true;
 				}
@@ -40,7 +43,7 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SettingTab(this.app, this));
 
 		this.registerCodeMirror((cm: CodeMirror.Editor) => {
 			console.log('codemirror', cm);
@@ -58,22 +61,61 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async loadSettings() {
+		this.getRulesets(this.app.vault.configDir + "/regex-pipleline-rules/");
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+	
+	async getRulesets(rulesetFolder : string) {
+		fs.readdir(path.resolve(rulesetFolder), (err, files) => {
+			this.rules = files;
+		});
+	}
+
+	applyRuleset (ruleset : string) {
+		let lineParser = /^"(.+)"\s?"(.+)"\n$/;
+		let ruleMatches = lineParser.exec(fs.readFileSync(ruleset).toString())
+		if (ruleMatches == null) return;
+		let activeMarkdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (activeMarkdownView == null) return;
+
+		console.log(ruleMatches);
+		let matchRule = new RegExp(ruleMatches[1]);
+		if (activeMarkdownView.editor.somethingSelected())
+		{
+			let newSelection = activeMarkdownView.editor.getSelection().replace(matchRule, ruleMatches[2]);
+			activeMarkdownView.editor.replaceSelection(newSelection)
+		}
+		else
+		{
+			let newFullNote = activeMarkdownView.data.replace(matchRule, ruleMatches[2]);
+		}
+
+		activeMarkdownView.requestSave();
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class ApplyRuleSetMenu extends Modal {
+	plugin: MyPlugin;
+	constructor(app: App, plugin: MyPlugin) {
 		super(app);
+		this.plugin = plugin;
 	}
 
 	onOpen() {
 		let {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('Rules are located at .obsidian/regex-pipeline-rulesets');
+		for (let i = 0; i < this.plugin.rules.length; i++)
+		{
+			new Setting(contentEl)
+				.setName(this.plugin.rules[i])
+				.addButton(btn => btn.onClick(async () => {
+					this.plugin.applyRuleset(this.plugin.rules[i])
+				}))
+		}
 	}
 
 	onClose() {
@@ -82,7 +124,8 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+
+class SettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -99,7 +142,7 @@ class SampleSettingTab extends PluginSettingTab {
 
 		new Setting(containerEl)
 			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setDesc('It\'s not a secret')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
 				.setValue('')
